bf39c79c57d0abdf8bedfdbadff4729cbe648090
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2025-07-19 19:31:10 +0600

    Add issueStoryPath method in FilePath.php

--- src/Config/FilePath.php
+++ src/Config/FilePath.php
@@ -7,4 +7,9 @@ class FilePath
     public const INPUT = BASE_DIR . '/files/input/';
     public const OUTPUT = BASE_DIR . '/files/output/';
     public const STORIES = BASE_DIR . '/files/stories/';
+
+    public static function issueStoryPath(string $issueKey, string $extension = '.md', string $suffix = ''): string
+    {
+        return self::STORIES . '/' . $issueKey . $suffix . $extension;
+    }
 }
